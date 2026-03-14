af3d943cb59f174b3cd894ce81547438b78239f6
Raja Bhagat <bhagatraja116@gmail.com>
2026-03-14 20:47:04 +0530

    dockerfile update for migration of prisma database

---
+++ dist/src/prisma.d.ts
@@ -0,0 +1,4 @@
+import { Prisma } from "./generated/prisma/client.js";
+declare const prisma: import("./generated/prisma/internal/class.js").PrismaClient<never, Prisma.GlobalOmitConfig | undefined, import("@prisma/client/runtime/client").DefaultArgs>;
+export default prisma;
+//# sourceMappingURL=prisma.d.ts.map
